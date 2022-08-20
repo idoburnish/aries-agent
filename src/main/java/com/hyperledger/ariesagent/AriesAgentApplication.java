070b27172336d5b19d39dfe978e70a4a7111ac63
idoburnish <doyun7433@naver.com>
2022-08-20 16:58:56 +0900

    init project

---
+++ src/main/java/com/hyperledger/ariesagent/AriesAgentApplication.java
@@ -0,0 +1,13 @@
+package com.hyperledger.ariesagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriesAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AriesAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyperledger/ariesagent/AriesAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyperledger.ariesagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AriesAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
